bb809b74f5d5d4c0e50ecd2735c5f7009522b417
Hunter Parcells <hunterparcells@gmail.com>
2023-05-26 00:00:02 -0400

    Start footer.

--- src/components/RewardsHeaderItem/RewardsHeaderItem.tsx
+++ src/components/RewardsHeaderItem/RewardsHeaderItem.tsx
@@ -23,8 +23,8 @@ function DropdownSvg() {
   return (
     <svg width='8' height='4' viewBox='0 0 8 4' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M0.175484 0.896965C-0.202101 0.563595 0.0698943 0 0.608408 0L7.39151 0C7.93494 0 8.20477 0.572243 7.81739 0.903186L4.36954 3.84856C4.13043 4.05285 3.74596 4.0501 3.51086 3.84245L0.175484 0.896965Z'
         fill='#546BFF'
       />
