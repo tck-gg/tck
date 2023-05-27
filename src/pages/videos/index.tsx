5ee8a29708c59776679ebf2b16eec5657ebb58c9
Hunter Parcells <hunterparcells@gmail.com>
2023-05-27 03:01:30 -0400

    Skeleton videos page.

--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -1,10 +1,43 @@
 import Layout from '@/components/Layout';
 import PageHeader from '@/components/PageHeader/PageHeader';
+import { faYoutube, faTiktok, faTwitch } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import classes from './videos.module.scss';
 
 function Videos() {
   return (
     <Layout title='Videos'>
       <PageHeader title='Videos' />
+      <div className={classes.sectionWrapper}>
+        <div className={classes.section}>
+          <p className={classes.social}>
+            <FontAwesomeIcon icon={faYoutube} className={classes.icon} color='#f81e1e' />
+            YouTube
+          </p>
+          <div className={classes.videoWrapper}>
+            <p>Stuff</p>
+          </div>
+        </div>
+        <div className={classes.section}>
+          <p className={classes.social}>
+            <FontAwesomeIcon icon={faTiktok} className={classes.icon} color='#ff1f64' />
+            TikTok
+          </p>
+          <div className={classes.videoWrapper}>
+            <p>Stuff</p>
+          </div>
+        </div>
+        <div className={classes.section}>
+          <p className={classes.social}>
+            <FontAwesomeIcon icon={faTwitch} className={classes.icon} color='#8f46fb' />
+            Twitch
+          </p>
+          <div className={classes.videoWrapper}>
+            <p>Stuff</p>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
