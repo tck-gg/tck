945f005c0217b13fa3c3268bcb556da3f5e32d80
Hunter Parcells <hunterparcells@gmail.com>
2023-06-01 15:04:39 -0400

    Added Google Analytics for later.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CookiesProvider } from 'react-cookie';
+import ReactGA from 'react-ga4';
 
 import { ProvideAuth } from '@/hooks/auth';
 
@@ -8,6 +10,17 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.scss';
 
 function App({ Component, pageProps }: AppProps) {
+  // useEffect(() => {
+  //   // Google Analytics
+  //   if (process.env.NODE_ENV === 'production') {
+  //     ReactGA.initialize('G-PPF6YEH1XJ');
+  //     ReactGA.send({
+  //       hitType: 'pageview',
+  //       page: window.location.pathname
+  //     });
+  //   }
+  // }, []);
+
   return (
     <>
       <Head>
