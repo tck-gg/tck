9484593bf4816056ac5a4700dc0f0034514b7a4d
Hunter Parcells <hunterparcells@gmail.com>
2023-06-05 22:01:48 -0400

    Added page header gradient.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import Page from '../Page';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import PageHeaderGlow from '../PageHeaderGlow/PageHeaderGlow';
 
 import classes from './Layout.module.scss';
 
 function Layout({ title, children }: { title?: string; children: React.ReactNode }) {
   return (
     <Page title={title}>
+      <PageHeaderGlow />
       <Header />
       <div className={classes.content}>{children}</div>
       <Footer />
