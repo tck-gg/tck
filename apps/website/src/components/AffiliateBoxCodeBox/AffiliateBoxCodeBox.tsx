8833d0d3a3a59a477b335c79503b1d034ec5de8f
Hunter Parcells <hunterparcells@gmail.com>
2023-06-07 15:01:17 -0400

    Affiliate code copied effect.

--- apps/website/src/components/AffiliateBoxCodeBox/AffiliateBoxCodeBox.tsx
+++ apps/website/src/components/AffiliateBoxCodeBox/AffiliateBoxCodeBox.tsx
@@ -1,20 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Copy from '../svg/Copy';
 import Jagged from '../svg/Jagged';
 
 import classes from './AffiliateBoxCodeBox.module.scss';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import clsx from 'clsx';
 
 function AffiliateBoxCodeBox({ children }: { children: string }) {
+  const [hasCopied, setHasCopied] = useState(false);
+
   function handleClick() {
     navigator.clipboard.writeText(children);
+    setHasCopied(true);
   }
+
+  useEffect(() => {
+    if (hasCopied) {
+      setTimeout(() => {
+        setHasCopied(false);
+      }, 1500);
+    }
+  }, [hasCopied]);
 
   return (
     <div className={classes.root}>
       <Jagged className={classes.background} />
+
       <span className={classes.code}>{children}</span>
-      <span onClick={handleClick} className={classes.copy}>
-        <Copy />
-      </span>
+
+      {hasCopied ? (
+        <FontAwesomeIcon
+          icon={faCheck}
+          className={clsx(classes.copy, classes.copied)}
+          color='#3fb950'
+        />
+      ) : (
+        <span onClick={handleClick} className={classes.copy}>
+          <Copy />
+        </span>
+      )}
     </div>
   );
 }
