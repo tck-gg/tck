fbd1c9316883b0d2f4f0183331863b52ca749e12
Hunter Parcells <hunterparcells@gmail.com>
2023-06-07 16:33:17 -0400

    Added affiliates to home page.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TCK
 > Affiliate website for TCK.
+
+![](https://wakatime.com/badge/user/6b7d9181-edde-4a25-857c-e7101bfee7ea/project/37e24fb3-b1db-4114-a105-eb5412ae4287.svg?style=for-the-badge)
+
+## `.env`
+The main website under `/apps/website` requires a `.env` file in its workspace root. Details included in the workspace's [README.md](./apps/website/README.md).
--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -4,6 +4,9 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 import Layout from '@/components/Layout/Layout';
 import Button from '@/components/Button/Button';
+import AffiliateBox from '@/components/AffiliateBox/AffiliateBox';
+
+import { AFFILIATES } from '@/data/affiliates';
 
 import classes from './index.module.scss';
 
@@ -31,18 +34,58 @@ function Home() {
             >
               Explore Rewards
             </Button>
-            <Button variant='primary' rightIcon={faAngleRight}>
+            <Button
+              variant='primary'
+              rightIcon={faAngleRight}
+              onClick={() => {
+                router.push('/videos');
+              }}
+            >
               Watch
             </Button>
           </div>
         </div>
         <div className={classes.section}>
-          <p className={classes.sectionDescription}>Affiliate Codes</p>
-          <p className={classes.sectionTitle}>Free Rewards</p>
+          <div className={classes.sectionHeader}>
+            <p className={classes.sectionDescription}>Affiliate Codes</p>
+            <p className={classes.sectionTitle}>Free Rewards</p>
+          </div>
+          <div className={classes.affiliateWrapper}>
+            {AFFILIATES.filter((affiliate) => {
+              return affiliate.featured;
+            })
+              .map((affiliate) => {
+                return (
+                  <AffiliateBox
+                    key={affiliate.name}
+                    image={affiliate.image}
+                    reward={affiliate.reward}
+                    name={affiliate.name}
+                    codes={affiliate.codes}
+                    tags={affiliate.tags}
+                    featured={affiliate.featured}
+                  />
+                );
+              })
+              .splice(0, 4)}
+          </div>
+          <Button
+            rightIcon={faAngleRight}
+            onClick={() => {
+              router.push('/affiliates');
+            }}
+          >
+            Discover More
+          </Button>
         </div>
         <div className={classes.section}>
-          <p className={classes.sectionDescription}>Stay Up to Date</p>
-          <p className={classes.sectionTitle}>More TCK Content</p>
+          <div className={classes.sectionHeader}>
+            <p className={classes.sectionDescription}>Stay Up to Date</p>
+            <p className={classes.sectionTitle}>More TCK Content</p>
+          </div>
+          <div>
+            <p>TODO</p>
+          </div>
         </div>
       </div>
     </Layout>
