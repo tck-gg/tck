80303b1eabc4181f7257574d8d9c58822b003607
Hunter Parcells <hunterparcells@gmail.com>
2023-06-07 20:58:45 -0400

    Added content background SVG.

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -120,6 +120,31 @@ function Home() {
           </Button>
         </div>
         <div className={classes.section}>
+          <svg
+            className={classes.sectionHeaderSvg}
+            width='329'
+            height='127'
+            viewBox='0 0 329 127'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M2.74369 9.28527C1.51705 7.34478 2.83558 4.8018 5.12834 4.68611L91.6924 0.318269C94.2175 0.190858 95.7581 3.05253 94.2615 5.09028L46.2553 70.455C45.0042 72.1585 42.4309 72.0687 41.3015 70.2821L2.74369 9.28527Z'
+              fill='#171729'
+              fill-opacity='0.55'
+            />
+            <path
+              d='M326.797 78.4488C327.781 80.0012 326.727 82.0382 324.891 82.1306L253.054 85.7457C251.032 85.8474 249.8 83.5553 251 81.9256L290.84 27.8258C291.841 26.4663 293.895 26.5381 294.799 27.9641L326.797 78.4488Z'
+              fill='#171729'
+              fill-opacity='0.55'
+            />
+            <path
+              d='M60.6622 95.0919C59.1652 94.7593 58.5841 92.9351 59.6128 91.7978L98.2042 49.1342C99.3367 47.8822 101.415 48.5099 101.665 50.1795L109.605 103.181C109.813 104.569 108.564 105.734 107.193 105.43L60.6622 95.0919Z'
+              fill='#171729'
+              fill-opacity='0.55'
+            />
+          </svg>
+
           <div className={classes.sectionHeader}>
             <p className={classes.sectionDescription}>Stay Up to Date</p>
             <p className={classes.sectionTitle}>More TCK Content</p>
