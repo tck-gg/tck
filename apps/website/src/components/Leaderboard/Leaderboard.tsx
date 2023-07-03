ccfa1f75632b1379e045e981befaff1cf7c43bf6
Hunter Parcells <hunterparcells@gmail.com>
2023-07-03 13:15:38 -0400

    Leaderboard 4-10 display.

---
+++ apps/website/src/components/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,38 @@
+import clsx from 'clsx';
+import { Prisma } from 'database';
+
+import classes from './Leaderboard.module.scss';
+
+export type ILeaderboard = Prisma.LeaderboardGetPayload<{
+  include: { spots: true };
+}>;
+
+function Leaderboard({ leaderboard }: { leaderboard: ILeaderboard }) {
+  return (
+    <div className={classes.root}>
+      <table>
+        <tr>
+          <th>User</th>
+          <th>Wagered</th>
+        </tr>
+        {leaderboard.spots
+          .map((spot, index) => {
+            return (
+              <tr className={classes.item} key={spot.id}>
+                <td className={clsx(classes.grow, classes.user)}>
+                  <span className={classes.spot}>#{index + 1}</span>
+                  <span>{spot.username}</span>
+                </td>
+                <td className={clsx(classes.shrink, classes.amount)}>
+                  ${spot.amount.toLocaleString('en-US')}
+                </td>
+              </tr>
+            );
+          })
+          .splice(3, 10)}
+      </table>
+    </div>
+  );
+}
+
+export default Leaderboard;
--- apps/website/src/pages/leaderboards/index.tsx
+++ apps/website/src/pages/leaderboards/index.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { Prisma, getAllLeaderboards } from 'database';
+import { LeaderboardType } from 'types';
 
 import Layout from '@/components/Layout/Layout';
 
 import PageHeader from '@/components/PageHeader/PageHeader';
-import { useState } from 'react';
-import { LeaderboardType } from 'types';
+import Leaderboard from '@/components/Leaderboard/Leaderboard';
+
+import classes from './leaderboards.module.scss';
 
 export type ILeaderboard = Prisma.LeaderboardGetPayload<{
   include: { spots: true };
@@ -27,19 +30,21 @@ function Leaderboards({
 
   return (
     <Layout title='Leaderboards'>
-      <PageHeader title='Leaderboards' />
-      <select
-        onChange={(e) => {
-          setSelectedLeaderboard(e.target.value as LeaderboardType);
-        }}
-      >
-        <option value='stake'>Stake</option>
-        <option value='gamdom'>Gamdom</option>
-        <option value='csgoroll'>CSGORoll</option>
-        <option value='hypedrop'>HypeDrop</option>
-      </select>
-      <br />
-      {JSON.stringify(leaderboards[selectedLeaderboard])}
+      <div className={classes.root}>
+        <PageHeader title='Leaderboards' />
+        <select
+          onChange={(e) => {
+            setSelectedLeaderboard(e.target.value as LeaderboardType);
+          }}
+        >
+          <option value='stake'>Stake</option>
+          <option value='gamdom'>Gamdom</option>
+          <option value='csgoroll'>CSGORoll</option>
+          <option value='hypedrop'>HypeDrop</option>
+        </select>
+
+        <Leaderboard leaderboard={leaderboards[selectedLeaderboard]} />
+      </div>
     </Layout>
   );
 }
