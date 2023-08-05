bbc42b3fc4642b3bacab5cbb5d177cadc4fcf02e
Hunter Parcells <hunterparcells@gmail.com>
2023-08-05 14:12:18 -0400

    Age verification for direct affiliates link.

--- apps/website/src/pages/affiliates/index.tsx
+++ apps/website/src/pages/affiliates/index.tsx
@@ -1,6 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+
 import AffiliateBox from '@/components/AffiliateBox/AffiliateBox';
 import Layout from '@/components/Layout/Layout';
 import PageHeader from '@/components/PageHeader/PageHeader';
+
+import { useAgeVerification } from '@/hooks/age-verification';
 
 import { IAffiliate } from '@/types/affiliate';
 
@@ -9,24 +16,37 @@ import { AFFILIATES } from '@/data/affiliates';
 import classes from './affiliates.module.scss';
 
 function Affiliates() {
+  const [cookie, setCookie] = useCookies(['hasVerifiedAge']);
+  const [isVerified, setIsVerified] = useState(cookie.hasVerifiedAge === 'true');
+  const ageVerification = useAgeVerification();
+
+  useEffect(() => {
+    if (!isVerified) {
+      ageVerification.verify(() => {
+        setIsVerified(true);
+      });
+    }
+  }, []);
+
   return (
     <Layout title='Affiliates'>
       <PageHeader title='Affiliates' />
 
       <div className={classes.affiliateWrapper}>
-        {AFFILIATES.map((affiliate: IAffiliate) => {
-          return (
-            <AffiliateBox
-              key={affiliate.name}
-              image={affiliate.image}
-              reward={affiliate.reward}
-              name={affiliate.name}
-              codes={affiliate.codes}
-              tags={affiliate.tags}
-              featured={affiliate.featured}
-            />
-          );
-        })}
+        {isVerified &&
+          AFFILIATES.map((affiliate: IAffiliate) => {
+            return (
+              <AffiliateBox
+                key={affiliate.name}
+                image={affiliate.image}
+                reward={affiliate.reward}
+                name={affiliate.name}
+                codes={affiliate.codes}
+                tags={affiliate.tags}
+                featured={affiliate.featured}
+              />
+            );
+          })}
       </div>
     </Layout>
   );
