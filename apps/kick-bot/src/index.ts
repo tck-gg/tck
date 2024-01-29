75eddee703625da0c229da10a013a6c99cf35c7a
Hunter Parcells <hunterparcells@gmail.com>
2024-01-29 09:06:02 -0500

    fix: Prevent using channel specific emote in development

--- apps/kick-bot/src/index.ts
+++ apps/kick-bot/src/index.ts
@@ -101,10 +101,17 @@ if(process.env.NODE_ENV === 'production') {
       }
       currentRaffle = createdRaffleId;
 
-      await client.api.chat.sendMessage(
-        channel.data.chatroom.id,
-        `Raffle started for ${reward} points; type [emote:2191419:tckFREE] to join within the next ${duration} seconds!`
-      );
+      if(process.env.NODE_ENV === 'production') {
+        await client.api.chat.sendMessage(
+          channel.data.chatroom.id,
+          `Raffle started for ${reward} points; type [emote:2191419:tckFREE] to join within the next ${duration} seconds!`
+        );
+      }else {
+        await client.api.chat.sendMessage(
+          channel.data.chatroom.id,
+          `Raffle started for ${reward} points; type tckFREE to join within the next ${duration} seconds!`
+        );
+      }
       
       raffleTimeout = setTimeout(async () => {
         if(!currentRaffle) {
