af8e4e6abe8e382894ac4405c3c933eeafa306d6
Hunter Parcells <hunterparcells@gmail.com>
2024-04-25 13:58:16 -0400

    feat: Profile connections save changes button.

--- apps/website/src/components/profile/tab/ProfileTabConnections/ProfileTabConnections.tsx
+++ apps/website/src/components/profile/tab/ProfileTabConnections/ProfileTabConnections.tsx
@@ -1,9 +1,20 @@
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import Button from '@/components/ui/Button/Button';
 import ProfileBoxBase from '@/components/profile/ProfileBoxBase/ProfileBoxBase';
 import VerificationKick from '@/components/profile/verification/VerificationKick/VerificationKick';
+
+import { useProfile } from '@/hooks/profile';
 
 import classes from './ProfileTabConnections.module.scss';
 
 function ProfileTabConnections() {
+  const profile = useProfile();
+
+  function close() {
+    profile.close();
+  }
+
   return (
     <>
       <div className={classes.group}>
@@ -15,6 +26,9 @@ function ProfileTabConnections() {
           <p>TODO</p>
         </ProfileBoxBase>
       </div>
+      <Button rightIcon={faChevronRight} onClick={close} variant='gradient'>
+        Save Changes
+      </Button>
     </>
   );
 }
