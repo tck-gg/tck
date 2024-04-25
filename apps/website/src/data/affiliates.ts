62d3b3b1475b3168510253fa22a869f9031020e2
Hunter Parcells <hunterparcells@gmail.com>
2024-04-25 13:24:38 -0400

    feat: Add PackDraw to featured affiliates. (TCK-167)

--- apps/website/src/data/affiliates.ts
+++ apps/website/src/data/affiliates.ts
@@ -4,7 +4,7 @@ import gamdomLogo from '../images/affiliate/gamdom.png';
 import stakeLogo from '../images/affiliate/stake.png';
 import hypeDropLogo from '../images/affiliate/hypedrop.png';
 import csgorollLogo from '../images/affiliate/csgoroll.png';
-
+import packDrawLogo from '../images/affiliate/packdraw.png';
 import datDropLogo from '../images/affiliate/datdrop.png';
 import rollbitLogo from '../images/affiliate/rollbit.png';
 import roobetLogo from '../images/affiliate/roobet.png';
@@ -15,18 +15,57 @@ import csgo500Logo from '../images/affiliate/csgo500.png';
  */
 export const AFFILIATES: IAffiliate[] = [
   {
-    image: gamdomLogo,
-    reward: '15% Instant Rakeback',
-    name: 'Gamdom',
+    image: roobetLogo,
+    reward: 'Exclusive rewards and instant rakeback!',
+    name: 'Roobet',
     codes: [
       {
         code: 'TCK',
-        link: 'https://gamdom.com/landing?tag1=TCK'
+        link: 'https://roobet.com/?ref=TCK'
       }
     ],
     tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
     featured: true
   },
+  {
+    image: datDropLogo,
+    reward: '5% Deposit Bonus',
+    name: 'DatDrop',
+    codes: [
+      {
+        code: 'TCK',
+        link: 'https://datdrop.com/'
+      }
+    ],
+    tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
+    featured: true
+  },
+  {
+    image: packDrawLogo,
+    reward: '5% Deposit Bonus + Bonus',
+    name: 'PackDraw',
+    codes: [
+      {
+        code: 'TCK',
+        link: 'https://packdraw.com/?ref=TCK'
+      }
+    ],
+    tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
+    featured: true
+  },
+  // {
+  //   image: gamdomLogo,
+  //   reward: '15% Instant Rakeback',
+  //   name: 'Gamdom',
+  //   codes: [
+  //     {
+  //       code: 'TCK',
+  //       link: 'https://gamdom.com/landing?tag1=TCK'
+  //     }
+  //   ],
+  //   tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
+  //   featured: true
+  // },
   {
     image: stakeLogo,
     reward: '10% Rakeback & VIP Rewards',
@@ -38,7 +77,7 @@ export const AFFILIATES: IAffiliate[] = [
       }
     ],
     tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
-    featured: true
+    featured: false
   },
   // {
   //   image: hypeDropLogo,
@@ -66,32 +105,6 @@ export const AFFILIATES: IAffiliate[] = [
   //   tags: ['CS:GO', 'Crypto', 'Battles', 'Originals'],
   //   featured: true
   // },
-  {
-    image: datDropLogo,
-    reward: '5% Deposit Bonus',
-    name: 'DatDrop',
-    codes: [
-      {
-        code: 'TCK',
-        link: 'https://datdrop.com/'
-      }
-    ],
-    tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
-    featured: false
-  },
-  {
-    image: roobetLogo,
-    reward: 'Exclusive rewards and instant rakeback!',
-    name: 'Roobet',
-    codes: [
-      {
-        code: 'TCK',
-        link: 'https://roobet.com/?ref=TCK'
-      }
-    ],
-    tags: ['Slots', 'Live Games', 'Originals', 'Crypto'],
-    featured: false
-  },
   {
     image: rollbitLogo,
     reward: '5% Rakeback & Daily/Weekly Cash Challenges',
